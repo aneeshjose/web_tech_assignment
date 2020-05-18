8b98be60100636543710ae66546225a76e5eb97c
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2020-05-18 16:53:56 +0530

    User successfully created

--- index.js
+++ index.js
@@ -17,11 +17,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.get('/', (req, res) =>{
+//     db.collection('authors').listDocuments().then(re) get()
+//   .then((snapshot) => {
+//     snapshot.forEach((doc) => {
+//       console.log(doc.id, '=>', doc.data());
+//     });
+//   })
+//   .catch((err) => {
+//     console.log('Error getting documents', err);
+//   });
+
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.post('/signupcheck', (req, res) =>{
+    var name=req.body.name;
+    var email=req.body.email;
+    var password=req.body.password;
+    var usertype=req.body.usertype;
+    db.collection(usertype).doc(email).get().then((snapshot)=>{
+        if(snapshot.exists){
+            res.send({'status':false,'code':'exists'});
+        }else{
+            db.collection(usertype).doc(email).set({'name':name,'password':password}).then((result)=>{
+                res.send({'status':true});
+            }).catch((e)=>{
+                res.send({'status':false,'code':e});
+            });
+        }
+    }).catch((e)=>{
+        console.log(e);
+        res.send({'status':false,'code':e});
+    })
 
-    res.send("{'status':'done'}");
+    // res.send("{'status':'done'}");
 });
     
 
