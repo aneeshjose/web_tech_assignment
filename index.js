a2911789abe891cc4da56a82652a4ef49cb40428
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2020-05-18 16:27:58 +0530

    user signuo request submitted:TODO process

--- index.js
+++ index.js
@@ -3,28 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var path = require('path');
+var bodyParser = require("body-parser");
+
 
 let serviceAccount = require('./adminsdk.json');
-
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 let db = admin.firestore();
-
-    // let docRef = db.collection('users').doc('alovelace');
-
-    // let setAda = docRef.set({
-    //   first: 'Ada',
-    //   last: 'Lovelace',
-    //   born: 1815
-    // });
-    // res.send('Done!');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+app.post('/signupcheck', (req, res) =>{
+
+    res.send("{'status':'done'}");
+});
+    
 
 app.get('/signup',(req,res)=>{
     res.sendFile(path.join(__dirname,'/signup.html'))
