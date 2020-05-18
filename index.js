6e3843f64c886cb8a72a6cb57f3ac16b2101f243
Aneesh Jose <31209870+aneeshjose@users.noreply.github.com>
2020-05-18 15:02:15 +0530

    landing page initialized

---
+++ index.js
@@ -0,0 +1,29 @@
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
+const port = 3000;
+var path = require('path');
+
+let serviceAccount = require('./adminsdk.json');
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+    // let docRef = db.collection('users').doc('alovelace');
+
+    // let setAda = docRef.set({
+    //   first: 'Ada',
+    //   last: 'Lovelace',
+    //   born: 1815
+    // });
+    // res.send('Done!');
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
